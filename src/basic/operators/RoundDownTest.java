e1b8bbb705484d9b27b99d5d52e6c4befff6fd63
junoade <ajchoi0928@gmail.com>
2022-07-02 23:13:04 +0900

    fix : Operator 복습

--- src/basic/operators/RoundDownTest.java
+++ src/basic/operators/RoundDownTest.java
@@ -5,6 +5,8 @@ public class RoundDownTest {
         float pi = 3.141592f;
         /* 소수점 세자리에서 내림하기 */
         float roundDownPi = (int)(pi * 1000) / 1000f; // 3145.92f 가 되었다가, 3145가 되고, /1000f 연산 수행
+        float roundPi = (int)(pi * 1000 + 0.5) / 1000f; // 0.5를 추가해줌
+
         /* Math 클래스의 round, ceil(올림), floor(내림) */
         float roundDownPi2 = (float) Math.floor(pi * 1000.0) /1000.0f;
 
@@ -12,6 +14,7 @@ public class RoundDownTest {
         String roundDownPi3 = String.format("%.3f", pi);
 
         System.out.printf("(int) 형변환을 이용한 내림 방법 : %f\n", roundDownPi);
+        System.out.printf("(int) 형변환을 이용한 반올림 방법 : %f\n", roundPi);
         System.out.printf("Math 유틸리티 활용한 내림 방법 : %f\n", roundDownPi2);
         System.out.printf("String format 메소드 활용한 반올림 방법 %s\n", roundDownPi3);
     }
