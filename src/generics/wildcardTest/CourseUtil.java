66ead5302dc64c64b529bcb2682aae0e079624ff
Junho Choi <ajchoi0928@gmail.com>
2022-09-17 16:12:05 +0900

    clean : 중복된 코드 제거

--- src/generics/wildcardTest/CourseUtil.java
+++ src/generics/wildcardTest/CourseUtil.java
@@ -5,18 +5,20 @@ import java.util.Arrays;
 public class CourseUtil {
     /* 모든 대상 */
     public static void getCourseInfo(Course<?> course){
-        System.out.println(course.getName() + " 수강생 리스트 : "
-                + Arrays.toString(course.getEnrollers()));
+        getInfo(course);
     }
 
     /* Student 부터, HighStudent 까지 */
     public static void getStudentCourseInfo(Course<? extends Student> course){
-        System.out.println(course.getName() + " 수강생 리스트 : "
-                + Arrays.toString(course.getEnrollers()));
+        getInfo(course);
     }
 
     /* Woker 부터, Person 까지 */
     public static void getWorkerCourseInfo(Course<? super Worker> course){
+        getInfo(course);
+    }
+
+    static void getInfo(Course<?> course){
         System.out.println(course.getName() + " 수강생 리스트 : "
                 + Arrays.toString(course.getEnrollers()));
     }
