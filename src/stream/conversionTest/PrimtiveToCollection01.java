7ceba66e50726563dba58fdca4418d390fbce299
Junho Choi <ajchoi0928@gmail.com>
2022-10-07 21:15:39 +0900

    review : primitive type arrays to Collection List

---
+++ src/stream/conversionTest/PrimtiveToCollection01.java
@@ -0,0 +1,10 @@
+package stream.conversionTest;
+
+import java.util.*;
+
+public class PrimtiveToCollection01 {
+    public static void main(String[] args) {
+        // ArrayList<Integer> list = (ArrayList<Integer>) Arrays.asList(1,2,3,4,5);
+        // System.out.println(list);
+    }
+}
